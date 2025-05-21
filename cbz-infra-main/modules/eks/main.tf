cdafca19d93d6f2a8950ab231a78efe2df7bdcd5
Akash Banda <akashbanda9@gmail.com>
2025-05-21 16:28:35 +0530

    Update main.tf

--- cbz-infra-main/modules/eks/main.tf
+++ cbz-infra-main/modules/eks/main.tf
@@ -1,13 +1,17 @@
-# Create an IAM Role for the EKS Cluster
+provider "aws" {
+  region = "us-east-1"
+}
+
+# IAM Role for the EKS Cluster
 resource "aws_iam_role" "eks_cluster_role" {
   name = "eks-cluster-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
         Principal = {
           Service = "eks.amazonaws.com"
         }
@@ -16,7 +20,7 @@ resource "aws_iam_role" "eks_cluster_role" {
   })
 }
 
-# Attach necessary policies to the EKS Cluster IAM Role
+# Attach policies to EKS Cluster Role
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
@@ -27,16 +31,16 @@ resource "aws_iam_role_policy_attachment" "eks_service_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
 }
 
-# Create an IAM Role for the Node Group
+# IAM Role for the Node Group
 resource "aws_iam_role" "eks_node_role" {
   name = "eks-node-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
         Principal = {
           Service = "ec2.amazonaws.com"
         }
@@ -45,7 +49,7 @@ resource "aws_iam_role" "eks_node_role" {
   })
 }
 
-# Attach necessary policies to the Node Group IAM Role
+# Attach policies to Node Group Role
 resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
   role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
@@ -61,27 +65,38 @@ resource "aws_iam_role_policy_attachment" "ec2_container_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-# Fetch the Default VPC and Subnets
+# Data for Default VPC and Subnets
 data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
 }
 
-# Create an EKS Cluster
+data "aws_subnet" "public_subnet_1a" {
+  id = data.aws_subnet_ids.default.ids[0]
+}
+
+data "aws_subnet" "public_subnet_1b" {
+  id = data.aws_subnet_ids.default.ids[1]
+}
+
+data "aws_subnet" "public_subnet_1c" {
+  id = data.aws_subnet_ids.default.ids[2]
+}
+
+# EKS Cluster
 resource "aws_eks_cluster" "cbz_cluster" {
   name     = "cbz-cluster"
   role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
-    subnet_ids =  aws_subnet.public_subnet_1a.id,
-    aws_subnet.public_subnet_1b.id,
-    aws_subnet.public_subnet_1c.id
+    subnet_ids = [
+      data.aws_subnet.public_subnet_1a.id,
+      data.aws_subnet.public_subnet_1b.id,
+      data.aws_subnet.public_subnet_1c.id
+    ]
   }
 
   depends_on = [
@@ -90,12 +105,16 @@ resource "aws_eks_cluster" "cbz_cluster" {
   ]
 }
 
-# Create a Node Group
+# EKS Node Group
 resource "aws_eks_node_group" "cbz_nodegroup" {
   cluster_name    = aws_eks_cluster.cbz_cluster.name
   node_group_name = "cbz-node-group"
   node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = data.aws_subnets.default.ids
+  subnet_ids      = [
+    data.aws_subnet.public_subnet_1a.id,
+    data.aws_subnet.public_subnet_1b.id,
+    data.aws_subnet.public_subnet_1c.id
+  ]
 
   scaling_config {
     desired_size = 2
@@ -112,8 +131,13 @@ resource "aws_eks_node_group" "cbz_nodegroup" {
   ]
 }
 
-# Output the kubeconfig
-# output "kubeconfig" {
-#   value = aws_eks_cluster.cbz_cluster.identity[0].oidc.issuer
-#   description = "Run 'aws eks update-kubeconfig --name cbz-cluster' to configure kubeconfig"
-# }
+# Output cluster endpoint and OIDC
+output "eks_cluster_endpoint" {
+  value       = aws_eks_cluster.cbz_cluster.endpoint
+  description = "EKS Cluster Endpoint"
+}
+
+output "kubeconfig_hint" {
+  value       = "Run this command: aws eks update-kubeconfig --name cbz-cluster"
+  description = "Kubeconfig setup command"
+}
