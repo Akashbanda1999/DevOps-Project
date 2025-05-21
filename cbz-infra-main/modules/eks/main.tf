71b227aade6ebac5ebccb5079558397b7ea7b31e
Akash Banda <akashbanda9@gmail.com>
2025-05-21 16:36:09 +0530

    Update main.tf

--- cbz-infra-main/modules/eks/main.tf
+++ cbz-infra-main/modules/eks/main.tf
@@ -1,92 +1,28 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-# IAM Role for the EKS Cluster
-resource "aws_iam_role" "eks_cluster_role" {
-  name = "eks-cluster-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-# Attach policies to EKS Cluster Role
-resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-  role       = aws_iam_role.eks_cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "eks_service_policy" {
-  role       = aws_iam_role.eks_cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-}
-
-# IAM Role for the Node Group
-resource "aws_iam_role" "eks_node_role" {
-  name = "eks-node-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-# Attach policies to Node Group Role
-resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-}
-
-resource "aws_iam_role_policy_attachment" "ec2_container_policy" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
-
-# Data for Default VPC and Subnets
+# Fetch Default VPC
 data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
+# Fetch All Subnets in the Default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
-data "aws_subnet" "public_subnet_1a" {
-  id = data.aws_subnet_ids.default.ids[0]
+# Get Individual Subnet Details
+data "aws_subnet" "subnet_1a" {
+  id = data.aws_subnets.default.ids[0]
 }
 
-data "aws_subnet" "public_subnet_1b" {
-  id = data.aws_subnet_ids.default.ids[1]
+data "aws_subnet" "subnet_1b" {
+  id = data.aws_subnets.default.ids[1]
 }
 
-data "aws_subnet" "public_subnet_1c" {
-  id = data.aws_subnet_ids.default.ids[2]
+data "aws_subnet" "subnet_1c" {
+  id = data.aws_subnets.default.ids[2]
 }
-
-# EKS Cluster
 resource "aws_eks_cluster" "cbz_cluster" {
   name     = "cbz-cluster"
   role_arn = aws_iam_role.eks_cluster_role.arn
