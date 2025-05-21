e9b0db555ebba8ec50873b6d03ace40d40c2f47a
Akash Banda <akashbanda9@gmail.com>
2025-05-21 15:55:52 +0530

    Update main.tf

--- cbz-infra-main/main.tf
+++ cbz-infra-main/main.tf
@@ -1,7 +1,7 @@
 # profile.tf
 provider "aws" {
   alias   = "test"
-  region  = "ap-south-1"              # Replace with your desired region
+  region  = "us-east-1"              # Replace with your desired region
   profile = "configs"   # Replace with the AWS CLI profile name you want to use
 }
 module "rds" {
