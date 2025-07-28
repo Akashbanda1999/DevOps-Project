29cdfa72623dc9bd3bed41069b616749acbf8c1a
Akash Banda <akashbanda9@gmail.com>
2025-07-28 13:51:47 +0530

    Update eks-cluster.tf

---
+++ aws-devops-crm/aws-devops-crm/terraform/eks/eks-cluster.tf
@@ -0,0 +1,8 @@
+# Terraform (terraform/eks/eks-cluster.tf)
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "crm-eks-cluster"
+  cluster_version = "1.24"
+  subnets         = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+}
